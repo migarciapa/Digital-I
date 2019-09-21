daa9eec8f198bad1615923e5474b12a37c64866d
migarciapa <55602810+migarciapa@users.noreply.github.com>
2019-09-20 20:28:56 -0500

    Suamdor y Comparador
    Se añaden los sumadores y comparadores de 1 bit y 4 bits.

---
+++ Practicas/Main.v
@@ -0,0 +1,8 @@
+`timescale 1ns / 1ps
+
+module Main();
+	//Este modulo se usara como principal para cargar y probar los proyectos. Este debe ser el modulo para la creacion del testbench.
+	//Los modulos creados deben ser almacenados en nuevos archivos de Module Verilog.
+	
+	
+endmodule
